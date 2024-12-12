1dd783b5449ff8aeb2f7225dd9ce0b55754d1c8f
Kristian <kristianwmurphy@gmail.com>
2024-12-12 18:26:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 # App.py
 
 from flask import Flask, render_template
+import git
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400
 def home():
     return render_template('index.html')
 
