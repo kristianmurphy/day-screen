0dc1dc786014a1a7feed54353cfd5f39996dc348
kristian <kristianwmurphy@gmail.com>
2025-04-10 10:46:03 +0100

    Update with functional parameters
    Added parameter viewing by subsystem, auto refreshing on timer, and started menu for alarms

--- app_backup.py
+++ app_backup.py
@@ -57,77 +57,55 @@ def fetch_param_values():
 #threading.Thread(target=fetch_param_values, daemon=True).start()
 #fetch_param_values()
 #param_values =fetch_column_names("value_table")
-#query_str = f"SELECT COUNT(*) FROM cx_table;"
-#results = VCC.Database.execute_raw_sql(query_str)
-#param_values = results
-
-@app.route("/table_partial")
-def table_partial():
-    page = int(request.args.get("page", 0))
-    subsystem = request.args.get("subsystem", "")
-    page_size=8
-
-    query_str = "SELECT c.description, v.cur_state, c.display_units FROM value_table v " \
-            "JOIN cx_table c ON v.parameter_id = c.parameter_id " \
-            "WHERE c.subsystem = '" + str(subsystem) + "' " \
-            "LIMIT " + str(page_size) + " OFFSET " + str(page * page_size) + ";"
-    results = VCC.Database.execute_raw_sql(query_str)
-    rows = []
-    for r in results:
-        rows.append({
-        "description": r[0],
-        "cur_state": r[1],
-        "display_units": r[2],
-    })
-        
-    # Query to get the total count of parameters for the given subsystem
-    #count_query = " SELECT COUNT(*) " \
-    #    "FROM value_table v " \
-    #    "JOIN cx_table c ON v.parameter_id = c.parameter_id "\
-    #    "WHERE c.subsystem = '" + str(subsystem)+ "' "
-    #count_results = VCC.Database.execute_raw_sql(count_query)
-    #total_rows = count_results[0][0] if count_results else 0
-    
-    # Calculate total pages based on the number of rows and the page size
-    total_pages = 1 #(total_rows + page_size - 1) // page_size  # Using ceiling division
-
-    return render_template(
-        "table.html",
-        rows=rows,
-        subsystem=subsystem,
-        page=page,
-        total_pages=total_pages
-    )
-
-
-
-@app.route("/alarm-test")
-def alarm_test():
-
-    str = "SELECT parameter_id, type FROM alarm_output_table;"
-    results = VCC.Database.execute_raw_sql(str)
-
-    #alarm_90101 = next((item for item in results if item[0] == 90101), None)
-
-    status = 0
-    if results:
-        status = 1
-        print("General Alarm:")
-        print(status)
-    else:
-        print("Alarm with ID 90101 not found.")
-
-
-    return render_template(
-        "alarm-test.html",
-        status = status
-    )
-
+query_str = f"SELECT COUNT(*) FROM cx_table;"
+results = VCC.Database.execute_raw_sql(query_str)
+param_values = results
 
 @app.route('/DAY_SCREEN', methods=['GET'])
 def DAY_SCREEN():
-    return render_template("index.html")
+    page_size = 20  # number of items per page
+    page = int(request.args.get('page', 0))  # page number starts at 0
+    subsystem = request.args.get('subsystem', 'Air Handlers')  # default subsystem
 
+    offset = page * page_size
+
+    query = """
+        SELECT c.parameter_id, c.value, c.subsystem, c.display_units
+        FROM cx_table c
+        JOIN description_table p ON c.parameter_id = p.parameter_id
+        WHERE c.subsystem = {subsystem}
+        ORDER BY c.parameter_id
+        LIMIT {page_size} OFFSET {offset};
+    """
+    results = VCC.Database.execute_raw_sql(query_str)
+
+    # Calculate the total number of pages based on the results
+    total_pages = math.ceil(len(results) / page_size) if results else 1
+
+    return render_template(
+        'index.html',
+        page=page,
+        rows=results,
+        total_pages=total_pages,
+        subsystem=subsystem
+    )
+
+
+@app.route('/DAY_SCREEN_test', methods=['GET', 'POST'])
+def CX_UI(): 
+    return render_template('index.html',
+                           sys_status='OK',
+                           in_control="HELM",
+                           wind="ENE 8kt (12kt)",
+                           air_status="Cooling",
+                           air_set=str(75),
+                           air_temp=str(75),
+                           battery_1=str(85),
+                           battery_2=str(86),
+                           fuel_tank=str(92),
+                           freshwater_tank=str(81),
+                           greywater_tank=str(10),
+                           blackwater_tank=(4))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port = 8000, threaded=True, debug=True) #, use_reloader=False)
